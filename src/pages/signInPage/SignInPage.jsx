d95149db528863e37cfd07170932c0b637ed04f9
joinsaeeddev <joinsaeeddev@gmail.com>
2024-06-28 15:19:09 +0500

    Login and Signup pages

--- src/pages/signInPage/SignInPage.jsx
+++ src/pages/signInPage/SignInPage.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React from "react";
+import Ellipse from "../../assets/image/home/Ellipse.png";
+import Subtract from "../../assets/image/home/Subtract.png";
+import Learn_Google_Icon from "../../assets/icons/Learn_Google_Icon.png";
+import facebook_icon from "../../assets/icons/facebook_icon.png";
 
 const SignInPage = () => {
   return (
-    <div>SignInPage</div>
-  )
-}
+    <div className="flex justify-center items-center w-full h-[100vh] py-24  relative">
+      <img
+        src={Ellipse}
+        alt=""
+        className="absolute left-0 h-[500px] hidden md:block"
+      />
+      <img
+        src={Subtract}
+        alt=""
+        className="absolute top-0 right-0 h-[350px] hidden md:block"
+      />
 
-export default SignInPage
+      <div className="text-center max-w-7xl">
+        <form action="" className="w-[400px] p-4">
+          <h2>Login</h2>
+          <div className="item border-b-2 w-full p-3">
+            <input
+              type="text"
+              placeholder="Username"
+              className="w-full outline-none"
+            />
+          </div>
+          <div className="item border-b-2 w-full p-3">
+            <input
+              type="text"
+              placeholder="Password"
+              className="w-full outline-none"
+            />
+          </div>
+          <div className="btn w-full bg-[#2B54FF] text-white py-2 mt-4">
+            <button>Sign up</button>
+          </div>
+          <p className="text-[#999999]">Or continue with</p>
+          <div className="flex items-center gap-2 justify-center">
+            <div className="rounded-full ">
+              <img
+                src={facebook_icon}
+                alt="Icon 1"
+                // className="h-12 w-12 "
+              />
+            </div>
+            <div className="rounded-full ">
+              <img
+                src={Learn_Google_Icon}
+                alt="Icon 1"
+                className="h-12 w-12 "
+              />
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignInPage;
